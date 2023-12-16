d6c6a8632d19704fb7c82892addee1f576e03136
Birchgrove <birchgrove0612@gmail.com>
2023-12-16 20:12:15 +0800

    add x86 config, add select config shell

---
+++ select_config.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ -z "${BUILD_PLATFORM}" ];then
+	echo "platform is nil, use x86.config"
+	cp x86.config .config
+	exit 0
+fi
+
+echo "platform is ${BUILD_PLATFORM}
+cp ${BUILD_PLATFORM}.config .config
